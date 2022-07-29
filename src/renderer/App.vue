ab04bbc814368584155c6a3e0e514fbf44bfd181
logeast <logeast@outlook.com>
2022-07-29 22:25:39 +0800

    feat: initial book branch

--- src/renderer/App.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang=ts>
-import Copyboards from './components/Copyboards.vue';
-</script>
-
-<template setup>
-  <copyboards></copyboards>
-</template>
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./src/renderer/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
